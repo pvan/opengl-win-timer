f7f3f94fe94067b7a66b887695beef4d960541df
Phil Vanderveen <philipvanderveen@gmail.com>
2017-12-03 20:01:06 -0500

    tabs to spaces

--- app.cpp
+++ app.cpp
@@ -22,20 +22,20 @@ float GetWallClockSeconds()
 
 
 
-char *vertex_shader = 
-	"#version 110 \n"
-	"void main()"
-	"{"
-	"	gl_Position = gl_Vertex;"
-	"}"
+char *vertex_shader =
+    "#version 110 \n"
+    "void main()"
+    "{"
+    "   gl_Position = gl_Vertex;"
+    "}"
 ;
 
-char *fragment_shader = 
-	"#version 110 \n"
-	"void main()"
-	"{"
-	"	gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0); \n"
-	"}"
+char *fragment_shader =
+    "#version 110 \n"
+    "void main()"
+    "{"
+    "   gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0); \n"
+    "}"
 ;
 
 
@@ -74,94 +74,94 @@ PFNGLACTIVETEXTUREPROC glActiveTexture;
 
 int init_opengl(HDC hdc)
 {
-	PIXELFORMATDESCRIPTOR pfd;
-	pfd.nSize = sizeof(PIXELFORMATDESCRIPTOR);
-	pfd.nVersion = 1;
-	pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER | PFD_SWAP_EXCHANGE;
-	pfd.iPixelType = PFD_TYPE_RGBA,
-	pfd.cColorBits = 32;
-	
-	int pixel_format = ChoosePixelFormat(hdc, &pfd);
-	SetPixelFormat(hdc, pixel_format, &pfd);
-	HGLRC hglrc = wglCreateContext(hdc);
-	wglMakeCurrent(hdc, hglrc);
-	
-	
-	glGenVertexArrays = (PFNGLGENVERTEXARRAYSPROC)wglGetProcAddress("glGenVertexArrays");
-	glGenBuffers = (PFNGLGENBUFFERSARBPROC)wglGetProcAddress("glGenBuffers");
-	glBindBuffer = (PFNGLBINDBUFFERPROC)wglGetProcAddress("glBindBuffer");
-	glBufferData = (PFNGLBUFFERDATAPROC)wglGetProcAddress("glBufferData");
-	glBindVertexArray = (PFNGLBINDVERTEXARRAYPROC)wglGetProcAddress("glBindVertexArray");
-	glEnableVertexAttribArray = (PFNGLENABLEVERTEXATTRIBARRAYPROC)wglGetProcAddress("glEnableVertexAttribArray");
-	glVertexAttribPointer = (PFNGLVERTEXATTRIBPOINTERPROC)wglGetProcAddress("glVertexAttribPointer");
-	
-	glCreateShader = (PFNGLCREATESHADERPROC)wglGetProcAddress("glCreateShader");
-	glShaderSource = (PFNGLSHADERSOURCEPROC)wglGetProcAddress("glShaderSource");
-	glCompileShader = (PFNGLCOMPILESHADERPROC)wglGetProcAddress("glCompileShader");
-	
-	glCreateProgram = (PFNGLCREATEPROGRAMPROC)wglGetProcAddress("glCreateProgram");
-	glAttachShader = (PFNGLATTACHSHADERPROC)wglGetProcAddress("glAttachShader");
-	glLinkProgram = (PFNGLLINKPROGRAMPROC)wglGetProcAddress("glLinkProgram");
-	glDeleteShader = (PFNGLDELETESHADERPROC)wglGetProcAddress("glDeleteShader");
-	glDeleteProgram = (PFNGLDELETEPROGRAMPROC)wglGetProcAddress("glDeleteProgram");
-	glGetShaderiv = (PFNGLGETSHADERIVPROC)wglGetProcAddress("glGetShaderiv");
-	glGetShaderInfoLog = (PFNGLGETSHADERINFOLOGPROC)wglGetProcAddress("glGetShaderInfoLog");
-	glGetProgramiv = (PFNGLGETPROGRAMIVPROC)wglGetProcAddress("glGetProgramiv");
-	glGetProgramInfoLog = (PFNGLGETPROGRAMINFOLOGPROC)wglGetProcAddress("glGetProgramInfoLog");
-	glUseProgram = (PFNGLUSEPROGRAMPROC)wglGetProcAddress("glUseProgram");
-	glGetUniformLocation = (PFNGLGETUNIFORMLOCATIONPROC)wglGetProcAddress("glGetUniformLocation");
-	glUniform4f = (PFNGLUNIFORM4FPROC)wglGetProcAddress("glUniform4f");
-	glUniformMatrix4fv = (PFNGLUNIFORMMATRIX4FVPROC)wglGetProcAddress("glUniformMatrix4fv");
-	glActiveTexture = (PFNGLACTIVETEXTUREPROC)wglGetProcAddress("glActiveTexture");
-	
-	
-	char shaderlog[256];
-	char displaybuf[256];
-	float before;
-	float after;
-		
-		
-	// create vertex shader program
-	GLuint vshader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vshader, 1, &vertex_shader, 0);   
-		before = GetWallClockSeconds();
-	glCompileShader(vshader);                 // <-- problem here (if first glCompileShader call)
-		after = GetWallClockSeconds();
-		glGetShaderInfoLog(vshader, 256, 0, shaderlog);	
-		sprintf(displaybuf, "seconds: %f\n\nvshader log:%s", after-before, shaderlog);
-		MessageBox(0, displaybuf, "glCompileShader vert", 0);
-		
-	// create fragment shader program
-	GLuint fshader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fshader, 1, &fragment_shader, 0);   
-		before = GetWallClockSeconds();
-	glCompileShader(fshader);                 // <-- problem here (if first glCompileShader call)
-		after = GetWallClockSeconds();
-		glGetShaderInfoLog(fshader, 256, 0, shaderlog);	
-		sprintf(displaybuf, "seconds: %f\n\nfhader log:%s", after-before, shaderlog);
-		//MessageBox(0, displaybuf, "glCompileShader frag", 0);
-	
-	// link shaders
-	GLuint shader_program = glCreateProgram();
-	glAttachShader(shader_program, vshader);
-	glAttachShader(shader_program, fshader);
-	glLinkProgram(shader_program);
-	glUseProgram(shader_program);
-	
-	
-	// pass triangle to gpu
-	float verts[] = {-1,-1, -1,1, 1,-1, 1,1};
-	GLuint vao, vbo;
-	glGenVertexArrays(1, &vao);
-	glGenBuffers(1, &vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
-	glBindVertexArray(vao);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), NULL);
-	
-	
-	return 0;
+    PIXELFORMATDESCRIPTOR pfd;
+    pfd.nSize = sizeof(PIXELFORMATDESCRIPTOR);
+    pfd.nVersion = 1;
+    pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER | PFD_SWAP_EXCHANGE;
+    pfd.iPixelType = PFD_TYPE_RGBA,
+    pfd.cColorBits = 32;
+
+    int pixel_format = ChoosePixelFormat(hdc, &pfd);
+    SetPixelFormat(hdc, pixel_format, &pfd);
+    HGLRC hglrc = wglCreateContext(hdc);
+    wglMakeCurrent(hdc, hglrc);
+
+
+    glGenVertexArrays = (PFNGLGENVERTEXARRAYSPROC)wglGetProcAddress("glGenVertexArrays");
+    glGenBuffers = (PFNGLGENBUFFERSARBPROC)wglGetProcAddress("glGenBuffers");
+    glBindBuffer = (PFNGLBINDBUFFERPROC)wglGetProcAddress("glBindBuffer");
+    glBufferData = (PFNGLBUFFERDATAPROC)wglGetProcAddress("glBufferData");
+    glBindVertexArray = (PFNGLBINDVERTEXARRAYPROC)wglGetProcAddress("glBindVertexArray");
+    glEnableVertexAttribArray = (PFNGLENABLEVERTEXATTRIBARRAYPROC)wglGetProcAddress("glEnableVertexAttribArray");
+    glVertexAttribPointer = (PFNGLVERTEXATTRIBPOINTERPROC)wglGetProcAddress("glVertexAttribPointer");
+
+    glCreateShader = (PFNGLCREATESHADERPROC)wglGetProcAddress("glCreateShader");
+    glShaderSource = (PFNGLSHADERSOURCEPROC)wglGetProcAddress("glShaderSource");
+    glCompileShader = (PFNGLCOMPILESHADERPROC)wglGetProcAddress("glCompileShader");
+
+    glCreateProgram = (PFNGLCREATEPROGRAMPROC)wglGetProcAddress("glCreateProgram");
+    glAttachShader = (PFNGLATTACHSHADERPROC)wglGetProcAddress("glAttachShader");
+    glLinkProgram = (PFNGLLINKPROGRAMPROC)wglGetProcAddress("glLinkProgram");
+    glDeleteShader = (PFNGLDELETESHADERPROC)wglGetProcAddress("glDeleteShader");
+    glDeleteProgram = (PFNGLDELETEPROGRAMPROC)wglGetProcAddress("glDeleteProgram");
+    glGetShaderiv = (PFNGLGETSHADERIVPROC)wglGetProcAddress("glGetShaderiv");
+    glGetShaderInfoLog = (PFNGLGETSHADERINFOLOGPROC)wglGetProcAddress("glGetShaderInfoLog");
+    glGetProgramiv = (PFNGLGETPROGRAMIVPROC)wglGetProcAddress("glGetProgramiv");
+    glGetProgramInfoLog = (PFNGLGETPROGRAMINFOLOGPROC)wglGetProcAddress("glGetProgramInfoLog");
+    glUseProgram = (PFNGLUSEPROGRAMPROC)wglGetProcAddress("glUseProgram");
+    glGetUniformLocation = (PFNGLGETUNIFORMLOCATIONPROC)wglGetProcAddress("glGetUniformLocation");
+    glUniform4f = (PFNGLUNIFORM4FPROC)wglGetProcAddress("glUniform4f");
+    glUniformMatrix4fv = (PFNGLUNIFORMMATRIX4FVPROC)wglGetProcAddress("glUniformMatrix4fv");
+    glActiveTexture = (PFNGLACTIVETEXTUREPROC)wglGetProcAddress("glActiveTexture");
+
+
+    char shaderlog[256];
+    char displaybuf[256];
+    float before;
+    float after;
+
+
+    // create vertex shader program
+    GLuint vshader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vshader, 1, &vertex_shader, 0);
+        before = GetWallClockSeconds();
+    glCompileShader(vshader);                 // <-- problem here (if first glCompileShader call)
+        after = GetWallClockSeconds();
+        glGetShaderInfoLog(vshader, 256, 0, shaderlog);
+        sprintf(displaybuf, "seconds: %f\n\nvshader log:%s", after-before, shaderlog);
+        MessageBox(0, displaybuf, "glCompileShader vert", 0);
+
+    // create fragment shader program
+    GLuint fshader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fshader, 1, &fragment_shader, 0);
+        before = GetWallClockSeconds();
+    glCompileShader(fshader);                 // <-- problem here (if first glCompileShader call)
+        after = GetWallClockSeconds();
+        glGetShaderInfoLog(fshader, 256, 0, shaderlog);
+        sprintf(displaybuf, "seconds: %f\n\nfhader log:%s", after-before, shaderlog);
+        //MessageBox(0, displaybuf, "glCompileShader frag", 0);
+
+    // link shaders
+    GLuint shader_program = glCreateProgram();
+    glAttachShader(shader_program, vshader);
+    glAttachShader(shader_program, fshader);
+    glLinkProgram(shader_program);
+    glUseProgram(shader_program);
+
+
+    // pass triangle to gpu
+    float verts[] = {-1,-1, -1,1, 1,-1, 1,1};
+    GLuint vao, vbo;
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
+    glBindVertexArray(vao);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), NULL);
+
+
+    return 0;
 }
 
 
@@ -173,30 +173,30 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     switch (message)
     {
-		case WM_CLOSE:
-			running = false;
-			break;
-		case WM_NCHITTEST:
-			LRESULT hit = DefWindowProc(hWnd, message, wParam, lParam);
-			if (hit == HTCLIENT)
-				hit = HTCAPTION;
-			return hit;
-			break;
+        case WM_CLOSE:
+            running = false;
+            break;
+        case WM_NCHITTEST:
+            LRESULT hit = DefWindowProc(hWnd, message, wParam, lParam);
+            if (hit == HTCLIENT)
+                hit = HTCAPTION;
+            return hit;
+            break;
     }
-	return DefWindowProc(hWnd, message, wParam, lParam);
+    return DefWindowProc(hWnd, message, wParam, lParam);
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	WNDCLASS wc = {0};
+    WNDCLASS wc = {0};
     wc.style = CS_HREDRAW | CS_VREDRAW;
-	wc.lpfnWndProc = WndProc;
+    wc.lpfnWndProc = WndProc;
     wc.hInstance = hInstance;
     wc.lpszClassName = "timer app";
     if (!RegisterClass(&wc)) { MessageBox(0, "RegisterClass failed", 0, 0); return 1; }
-	
+
     HWND hwnd = CreateWindowEx(
-		WS_EX_APPWINDOW | WS_EX_LAYERED,
+        WS_EX_APPWINDOW | WS_EX_LAYERED,
         "timer app", "title",
         WS_POPUP | WS_VISIBLE,
         CW_USEDEFAULT, CW_USEDEFAULT,
@@ -204,29 +204,29 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         0, 0, hInstance, 0);
     if (!hwnd) { MessageBox(0, "CreateWindowEx failed", 0, 0); return 1; }
     SetLayeredWindowAttributes(hwnd, 0, 180, LWA_ALPHA);
-	
-	
-	HDC hdc = GetDC(hwnd);
-	
-	init_opengl(hdc);
-	
-	
-	
-	while(running)
-	{
-		MSG msg;
-		while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
-		{
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
-		}
-		
-		glClearColor(0, 0.5, 0.7, 1);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-		SwapBuffers(hdc);
-		
-		Sleep(16);
-	}
+
+
+    HDC hdc = GetDC(hwnd);
+
+    init_opengl(hdc);
+
+
+
+    while(running)
+    {
+        MSG msg;
+        while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+
+        glClearColor(0, 0.5, 0.7, 1);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        SwapBuffers(hdc);
+
+        Sleep(16);
+    }
     return 0;
 }
